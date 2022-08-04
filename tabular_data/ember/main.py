94d8309f4fd0074f1125b7678357bd3f35802bb8
Aman Kumar <aman.kumar@alectio.com>
2022-08-04 10:13:10 -0700

    Fixed numeric classification: ember

--- tabular_data/ember/main.py
+++ tabular_data/ember/main.py
@@ -1,8 +1,9 @@
 import argparse
 import yaml
 import json
-from alectio_sdk.flask_wrapper import Pipeline
-from processes import train, test, infer, getdatasetstate
+# from alectio_sdk.flask_wrapper import Pipeline
+from alectio_sdk.sdk import Pipeline
+from processes_2 import train, test, infer, getdatasetstate
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config", help="Path to config.yaml", required=True)
@@ -12,17 +13,18 @@ with open(args.config, "r") as stream:
     args = yaml.safe_load(stream)
 
 # put the train/test/infer processes into the constructor
-app = Pipeline(
+App = Pipeline(
     name=args["exp_name"],
     train_fn=train,
     test_fn=test,
     infer_fn=infer,
     getstate_fn=getdatasetstate,
     args=args,
-    token='hPrjnEpRi0jLikGNNU8lIiCulCbdHAMJeIcBOc2XB4'
+    token='6c2369bd4c4845a79a3dc560153dd4d4'
 )
 
 if __name__ == "__main__":
     # payload = json.load(open(args["sample_payload"], "r"))
     # app._one_loop(args=args, payload=payload)
-    app(debug=True)
+    # app(debug=True)
+    App()
